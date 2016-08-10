f135c9da21ad491eb915f69b2e4ba8e4c76d676c
Ben Kauffman <ben.kauffman@developmentnow.com>
2016-08-10 15:47:11 -0700

    updated default webserver to allow outside traffic

--- pogom/utils.py
+++ pogom/utils.py
@@ -24,7 +24,7 @@ def parse_unicode(bytestring):
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-H', '--host', help='Set web server listening host', default='127.0.0.1')
+    parser.add_argument('-H', '--host', help='Set web server listening host', default='0.0.0.0')
     parser.add_argument('-P', '--port', type=int, help='Set web server listening port', default=5000)
 
     parser.add_argument('-d', '--debug', type=str.lower, help='Debug Level [info|debug]', default=None)
